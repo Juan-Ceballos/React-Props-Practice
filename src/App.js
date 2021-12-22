49f758966a4506e678b8efe0359547e9326a6918
Juan-Ceballos <juanceballos@pursuit.org>
2021-12-22 17:41:48 -0500

    practice using props, basic divs and headers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+/* Example 1, props, passing like parameters, html attribute syntax
+const App = () => {
+  let userDetails = {
+    name: "Jimmy B",
+    userId: 7348,
+    email: "jimmy@pursuit.org"
+  }
+  return <User name={userDetails.name} />
+}
 
-function App() {
+const User = (props) => {
+  const { name } = props
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="user-details">
+      <h1>{name}</h1>
     </div>
-  );
+  )
+}
+*/
+
+const App = () => {
+  let userDetails = {
+    name: "Jimmy B",
+    userId: 7348,
+    email: "jimmy@pursuit.org"
+  }
+
+  return <User name={userDetails.name} userId={userDetails.userId} />
+}
+
+const User = (props) => {
+  const { name, userId } = props
+  return (
+    <div className="user-details">
+      <h1>{name}</h1>
+      <h1>{userId}</h1>
+    </div>
+  )
 }
 
 export default App;
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
